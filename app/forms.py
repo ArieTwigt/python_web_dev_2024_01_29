12d44a835652d9d20b58ec4ff3b7b8e8aa9c6502
Arie Twigt <twigt.arie@gmail.com>
2024-01-28 09:59:45 +0100

    added additional numeric validation

--- app/forms.py
+++ app/forms.py
@@ -1,11 +1,34 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, ValidationError
+
+
+
+# custom validators
+def validate_country_code(self, field):
+    if field.data.isdigit():
+        raise ValidationError('Country code must not be numeric.')
+
+def validate_city(self, field):
+    if field.data.isdigit():
+        raise ValidationError('City name must not be numeric.')
+
+def NonNumeric():
+    message = 'Field must not be numeric.'
+
+    def _non_numeric(form, field):
+        if field.data.isdigit():
+            raise ValidationError(message)
+
+    return _non_numeric
 
 
 # prediction request form
 class PredictionRequestForm(FlaskForm):
     country_code = StringField('Country Code')
-    city = StringField('City', validators=[DataRequired(),
-                                           Length(min=0, max=80)])
+    city = StringField('City', validators=[DataRequired(message="Insert a city name"),
+                                           Length(min=0, max=80),
+                                           NonNumeric()])
     submit = SubmitField('Submit')
+
+    
--- app/routes.py
+++ app/routes.py
@@ -12,26 +12,32 @@ def index():
 
     # if we do a POST request
     if request.method == 'POST':
-        # get the data from the form
-        data = request.form
 
-        # create a new prediction request object: <TODO> add username
-        selected_country_code = data['country_code']
-        selected_city = data['city']
-        prediction_request = PredictionRequest(username="test",
-                                               country_code=selected_country_code,
-                                               city=selected_city)
-        
+        # validate the form
+        if form.validate_on_submit():
+            # get the data from the form
+            data = request.form
+
+            # create a new prediction request object: <TODO> add username
+            selected_country_code = data['country_code']
+            selected_city = data['city']
+            prediction_request = PredictionRequest(username="test",
+                                                country_code=selected_country_code,
+                                                city=selected_city)
+            
 
 
-        # save the new prediction request object        
-        try:
-            db.session.add(prediction_request)
-            db.session.commit()
-            flash(f'Saved prediction request for country code: <b>{selected_country_code}</b> and city: <b>{selected_city}</b>', 'alert alert-success')
-        except IntegrityError:
-            db.session.rollback()
-            flash(f'The prediction combination for country: <b>{selected_country_code}</b> and city: <b>{selected_city}</b>', 'alert alert-danger')
+            # save the new prediction request object        
+            try:
+                db.session.add(prediction_request)
+                db.session.commit()
+                flash(f'Saved prediction request for country code: <b>{selected_country_code}</b> and city: <b>{selected_city}</b>', 'alert alert-success')
+            except IntegrityError:
+                db.session.rollback()
+                flash(f'The prediction combination for country: <b>{selected_country_code}</b> and city: <b>{selected_city}</b>', 'alert alert-danger')
+        else:
+            flash(f'Wrong', 'alert alert-danger')
+            return render_template('index.html', form=form)
     
         # return to the template
         return redirect(url_for('index'))
